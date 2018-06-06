19592d5365b38cd17aa55207574b21c3e55d6f89
Carlos Yael Canizales Castro <cyaelcc94@gmail.com>
2018-06-06 15:34:07 -0500

    Added LED light in 3B

--- Raspberry/3B_Distance_Sensor.py
+++ Raspberry/3B_Distance_Sensor.py
@@ -3,19 +3,43 @@ import time
 GPIO.setmode(GPIO.BOARD)
 TRIG = 40
 ECHO = 38
+#	   R  G  B
+LED = [33,31,29]
 
+GPIO.setup(LED,GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(TRIG, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(ECHO, GPIO.IN)
 
-GPIO.output(TRIG,GPIO.HIGH)
-time.sleep(0.00001)
-GPIO.output(TRIG,GPIO.LOW)
-while GPIO.input(ECHO) ==0:
-	pulseStart = time.time()
-while GPIO.input(ECHO) ==1:
-	pulseEnd = time.time()
-pulseDuration = pulseEnd - pulseStart
-distance = pulseDuration * 17150
-distance = round(distance, 2)
-print "Distancia: "+str(distance)+" cm"
+
+try:
+	while True:
+		GPIO.output(TRIG,GPIO.HIGH)
+		time.sleep(0.00001)
+		GPIO.output(TRIG,GPIO.LOW)
+		while GPIO.input(ECHO) ==0:
+			pulseStart = time.time()
+		while GPIO.input(ECHO) ==1:
+			pulseEnd = time.time()
+		pulseDuration = pulseEnd - pulseStart
+		distance = pulseDuration * 17150
+		distance = round(distance, 2)
+
+		if distance < 10:
+			#RED LIGHT
+			GPIO.output(LED[0],GPIO.HIGH)
+		elif distance > 10 and distance < 20:
+			#YELLOW LIGHT
+			GPIO.output(LED[0],GPIO.HIGH)
+			GPIO.output(LED[1],GPIO.HIGH)			
+		else:
+			#GREEN LIGHT
+			GPIO.output(LED[1],GPIO.HIGH)
+
+
+		print "Distancia: "+str(distance)+" cm"
+		time.sleep(2)
+		GPIO.output(LED,GPIO.LOW)
+except Exception as e:
+	raise e
+print("The End")
 GPIO.cleanup()
