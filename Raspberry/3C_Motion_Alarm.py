01bc5f3c8c11eae5cd2a6ca94541772eaeacd229
Carlos Yael Canizales Castro <cyaelcc94@gmail.com>
2018-06-07 12:51:05 -0500

    3C Working

--- Raspberry/3C_Motion_Alarm.py
+++ Raspberry/3C_Motion_Alarm.py
@@ -19,33 +19,36 @@ pwmServo.start(0)
 
 def SetAngle(angle):
 	duty = angle / 18 +2
-	GPIO.output(servo, GPIO.HIGH)
+	GPIO.output(SERVO, GPIO.HIGH)
 	pwmServo.ChangeDutyCycle(duty)
-	sleep(1)
-	GPIO.output(servo, GPIO.LOW)
-	#pwmServo.ChangeDutyCycle(0)
+	time.sleep(1)
+	GPIO.output(SERVO, GPIO.LOW)
+	pwmServo.ChangeDutyCycle(0)
 
 def getDistance():
-		GPIO.output(TRIG,GPIO.HIGH)
-		time.sleep(0.00001)
-		GPIO.output(TRIG,GPIO.LOW)
-		while GPIO.input(ECHO) ==0:
-			pulseStart = time.time()
-		while GPIO.input(ECHO) ==1:
-			pulseEnd = time.time()
-		pulseDuration = pulseEnd - pulseStart
-		distance = pulseDuration * 17150
-		distance = round(distance, 2)
-		return distance
+	GPIO.output(TRIG,GPIO.HIGH)
+	#time.sleep(0.00001)
+	time.sleep(0.001)
+	GPIO.output(TRIG,GPIO.LOW)
+	while GPIO.input(ECHO) == 0:
+		pulseStart = time.time()
+	while GPIO.input(ECHO) ==1:
+		pulseEnd = time.time()
+	pulseDuration = pulseEnd - pulseStart
+	distance = round(pulseDuration * 17150,2)
+	return distance
 if __name__ == '__main__':
 
 	try:
+		print ("Start")
 		while True:
-
 			distance = getDistance()
 			if distance < 10:
 				#RED LIGHT
+				#print ("RED LIGHT")
 				GPIO.output(LED[0],GPIO.HIGH)
+				GPIO.output(LED[1],GPIO.LOW)
+				GPIO.output(LED[2],GPIO.LOW)
 				GPIO.output(BUZZER,GPIO.HIGH)
 				SetAngle(45)
 				time.sleep(.1)
@@ -53,16 +56,19 @@ if __name__ == '__main__':
 				SetAngle(135)
 			elif distance > 10 and distance < 20:
 				#YELLOW LIGHT
+				#print ("YELLOW LIGHT")
 				GPIO.output(LED[0],GPIO.HIGH)
 				GPIO.output(LED[1],GPIO.HIGH)			
+				GPIO.output(LED[2],GPIO.LOW)
 			else:
 				#GREEN LIGHT
+				#print ("GREEN LIGHT")
+				GPIO.output(LED[0],GPIO.LOW)
 				GPIO.output(LED[1],GPIO.HIGH)
+				GPIO.output(LED[2],GPIO.LOW)
 
-
-			print "Distancia: "+str(distance)+" cm"
-			time.sleep(2)
-			GPIO.output(LED,GPIO.LOW)
+			#print "Distancia: "+str(distance)+" cm"
+			time.sleep(.1)
 	except KeyboardInterrupt:
 		pass
 	print("The End")
