aaf763501d14ed4cb85e677bf40d9ee21a913a4f
Carlos Yael Canizales Castro <cyaelcc94@gmail.com>
2018-06-19 18:35:38 -0500

    Added the number of seconds from the program started

--- Raspberry/4A_LCD_HelloWorld.py
+++ Raspberry/4A_LCD_HelloWorld.py
@@ -1,14 +1,17 @@
 import RPi.GPIO as GPIO
 from RPLCD.gpio import CharLCD
+import time
 
 try:
-
+	initTime = time.time()
 	lcd = CharLCD(cols=16, rows=2,pin_rw=None,pin_rs=37,pin_e=35,pins_data=[33,31,29,23],numbering_mode=GPIO.BOARD)
 	lcd.crlf()	
 	lcd.cursor_pos = (0,0)
 	lcd.write_string('Hello World')
-	lcd.cursor_pos = (1, 1)
-	lcd.write_string('This is line 2')
+	while True:
+		lcd.cursor_pos = (1,0)
+		lcd.write_string(str(round(time.time()-initTime)))
+		time.sleep(.998)
 
 except KeyboardInterrupt:
 	pass
