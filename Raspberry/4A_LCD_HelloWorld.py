aed8cef16f14422dca7ea0e7619aa75d8ac3702e
Carlos Yael Canizales Castro <cyaelcc94@gmail.com>
2018-06-22 15:39:29 -0500

    Fixed clean lcd

--- Raspberry/4A_LCD_HelloWorld.py
+++ Raspberry/4A_LCD_HelloWorld.py
@@ -5,7 +5,7 @@ import time
 try:
 	initTime = time.time()
 	lcd = CharLCD(cols=16, rows=2,pin_rw=None,pin_rs=37,pin_e=35,pins_data=[33,31,29,23],numbering_mode=GPIO.BOARD)
-	lcd.crlf()	
+	lcd.clear()	
 	lcd.cursor_pos = (0,0)
 	lcd.write_string('Hello World')
 	while True:
